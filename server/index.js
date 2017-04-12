51dee072478a4c1d15c4879d5124851c7184be4b
Andre Duarte <andremacielduarte@gmail.com>
2017-04-12 01:33:20 +0100

    Add request parameters

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var app = express()
 var Discogs = require('disconnect').Client
+var admin = require("firebase-admin");
+var serviceAccount = require('./discogs-digger-1bd36-firebase-adminsdk-lxhwi-e5f19dddcb.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://discogs-digger-1bd36.firebaseio.com"
+});
 
 function getInventory(user) {
-  
   return new Promise(resolve => {
     function finish() {
       return Promise.all(promises).then(all => {
@@ -14,27 +20,14 @@ function getInventory(user) {
             const artist = listing.release.description.substring(0, limit)
             const title = listing.release.description.substring(limit + 3)
 
-            let media = []
-            if (title.includes('LP') || title.includes('12"') || title.includes('10"') || title.includes('7"')) {
-                media.push('vinyl')
-            }
-            if (title.includes('CD')) {
-                media.push('CD')
-            }
-            if (title.includes('DVD')) {
-                media.push('DVD')
-            }
-
             data.set(listing.release.id, {
               condition: listing.condition,
               price: listing.price.value,
               currency: listing.price.currency,
-              description: listing.release.description,
               releaseId: listing.release.id,
               uri: listing.uri,
               artist,
-              title,
-              media
+              title
             })  
           })
         })
@@ -68,52 +61,22 @@ function getInventory(user) {
       return getNextPage(1, lastPage);
     })
   })
-  
-  
-  
 }
 
-app.get('/', function (req, res) {
-  // var col = new Discogs().user().collection().getReleases('onemanclap', 0, {
-  //   per_page: 1000
-  // })
-  // col.then(res => {
-  //   console.log(res.releases.length)
-  // })
+app.get('/dig/:buyer/:seller', async (req, res) => {
+  const {buyer, seller} = req.params
+  const dbRef = admin.database().ref(`inventory/${seller}`)
 
-  // var cenas = new Discogs().marketplace().getInventory('MUNSTERAMA', {
-  //   per_page: 1000
-  // })
-  // cenas.then(res => {
-  //   console.log('1')
-  //   const lastPage = Math.ceil(res.pagination.items / res.pagination.per_page);
-  //   const promises = [cenas];
-  //   let a = 1;
-  //   for (let i = 1; i <= lastPage; i++) {
-  //     promises.push(new Discogs().marketplace().getInventory('MUNSTERAMA', {
-  //       per_page: 1000,
-  //       page: i
-  //     }).then(r => {
-  //       console.log(i + 'done')
-  //       console.log(a++)
-  //     }, e => console.log(e)))
-  //   }
+  // get possible inventory from db
+  let inventory = (await dbRef.once('value')).val()
 
-  //   Promise.all(promises).then(r => console.log(r))
-    
-    
+  // if inventory does not exist, cache it
+  if (!inventory) {
+    inventory = await getInventory(seller)
+    dbRef.set(inventory)
+  }
 
-
-  //   console.log(lastPage)
-  // }, e => console.log(e))
-
-  // getInventory('schaerban').then(count => console.log(count))
-  getInventory('kaleidosmoker').then(inv => {
-    res.send(JSON.stringify(inv))
-    console.log(inv.length)
-  })
-
-  
+  res.json(inventory)
 })
 
 app.listen(3000, function () {
